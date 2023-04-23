5028d208fdcb9e95eeb31cbc222a04746fa6e2a2
Sofia Katzman <katzmansof@gmail.com>
2023-04-23 19:30:12 -0400

    added basic form

--- src/Create.js
+++ src/Create.js
@@ -3,9 +3,23 @@ import React from 'react'
 function Create(){
     return(
         <div>
-
-        <h1>TEST</h1>
-
+            <form> 
+                    <label>Project Name: </label>
+                    <input type = "text"/> <br></br>
+                    <label>Project Description: </label>
+                    <input type = "text"/><br></br>
+                    <label>Project API Link: </label>
+                    <input type = "text"/> <br></br>
+                    <label>Project Category: </label>
+                    <select> 
+                        <option value = "null">-</option>
+                        <option value = "animals">Animals </option>
+                        <option value = "anime">Anime </option>
+                        <option value = "anti-malware">Anti-Malware</option>
+                        <option value = "art-and-design">Art & Design</option>
+                        <option value = "blockchain">Blockchain</option>
+                    </select>
+                </form>
         </div>
     )
 }
