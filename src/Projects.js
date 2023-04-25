6a51ca82694aa466efd26402eafe4c1d960b2926
Sofia Katzman <katzmansof@gmail.com>
2023-04-24 21:55:40 -0400

    added fetch request to display all projects on cloud

--- src/Projects.js
+++ src/Projects.js
@@ -1,30 +1,34 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
 
 function Projects(){
 
-    console.log("you've reached Projects")
-
+    const [projects, setProjects] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:8000/projects")
+        .then(r => r.json())
+        .then(data => {
+            setProjects(data)
+            console.log(data)})
+    }, [])
+  
 return(
     <div> <h1>Welcome to your Projects Cloud! </h1>
     <p>Here are the projects you've saved: 
     </p>
-        <div className = "card">
-            <h3>Project Name</h3>
-            <p>Project Description will go here ...</p>
-            <small>API Category : Type as Link</small><br></br>
+
+    {projects.map(project => {
+        return(
+        <div className = "card" key = {project.id}> 
+            <h3>{project.name}</h3>
+            <p>{project.description}</p>
+            <h6><a href = {project.apiLink}>API Link: {project.apiName}</a></h6><br></br>
             <button>delete</button>
-                   </div>
+            </div>
+        )
+    })}
     </div>
 )
 
 }
 
 export default Projects
-
-
-//My layout for displaying projects will be: 
-//div holding project card 
-//h1 holding project name 
-//p holding project description 
-//small displaying API name 
